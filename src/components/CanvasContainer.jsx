19b2b09c45dceb5914a9ddcf1ac129b88a2f43f3
Shikibata <hloic@outlook.com>
2023-03-07 11:09:47 +0100

    part 4 ok

--- src/components/CanvasContainer.jsx
+++ src/components/CanvasContainer.jsx
@@ -1,11 +1,22 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef} from "react"
 export default function CanvasContainer() {
   return (
-      <Canvas>
-        <mesh position={[0, 0, 0]}>
-          <boxGeometry/>
-          <meshBasicMaterial color={"red"}/>
-        </mesh>
+      <Canvas className={"canvas"}>
+        <Box/>
       </Canvas>
+  )
+}
+
+function Box() {
+  const ref = useRef()
+  useFrame(() => {
+    ref.current.rotation.y += 0.01;
+  })
+  return (
+  <mesh ref={ref}>
+    <boxGeometry/>
+    <meshBasicMaterial color={"blue"}/>
+  </mesh>
   )
 }
